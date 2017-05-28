81b8a18deef86f426a56be3a9f705b91d95329b5
decordoba <danieldecordobagil@hotmail.com>
2017-05-28 17:53:03 -0500

    Added two easy algorithms for a Google interview

---
+++ compareStringLists_Google.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python2
+
+"""
+Easy challenge for an internship at Google. Took me 15 min to write it.
+(I should have invested a bit more time though, I did not optimize it as
+much as I should have, and it was quite obvious how to do it, I thought I'd
+have the chance to resubmit it again later, like in HackerRank, but I didn't').
+
+One string is strictly smaller than another when the frequency of occurrences
+of the smallest character is less than the frequency of occurrence of the smallest
+character in the comparison string.
+
+For example, string "debc" is smaller than string "aaa" because the smallest
+character in "debc" is "b", with a frequency of 1, and the smallest character
+in "aaa" is "a", but with a frequency of 3.
+
+Write a function solution(A, B) that, given a string A (which contains M strings
+separated by " ") and string B (which contains N strings separated by " "), return
+an array C of N integers. For J in 0..N-1, C[J] specifies the number of strings
+in A strictly smaller than B[J].
+
+Example:
+    A = "abcd aabc bd", B = "aaa aa"
+    Returns [3, 2] because all words in A are strictly smaller than "aaa"
+                           and "abcd" and "bd" are strictly smaller than "aa"
+
+Constraints: 1<=N<=10000, 1<=M<=10000, 1<=length_any_word_in_A_or_B<=10
+             All the words only have lowercase English alphabet letters
+"""
+
+import random
+import string
+from basic import *  # Find basic.py in https://github.com/decordoba/basic-python
+
+
+def calculateValue(s):
+    # s: string without spaces
+    smallest = None
+    num = 0
+    for c in s:
+        if smallest is None or c < smallest:
+            smallest = c
+            num = 1
+        elif c == smallest:
+            num += 1
+    return num
+
+def calculateString(A):
+    # A: string of words
+    Alist = A.split()
+    values = []
+    for a in Alist:
+        values.append(calculateValue(a))
+    return values
+
+def solution(A, B):
+    valuesA = calculateString(A)
+    valuesB = calculateString(B)
+    
+    result = []
+    for b in valuesB:
+        num = 0
+        for a in valuesA:
+            if a < b:
+                num += 1
+        result.append(num)
+    
+    return result
+    
+# I did not submit this, but it is possible to speed up the above
+# code with a hash table. Hopefully it is not necessary, as we have
+# at most 10000, so at most O(M*N +M + N) (this would produce O(11M + 11N))
+# Empirically, the time difference is really big! 7s become 0.05s in the worst case
+def solution_faster(A, B):
+    valuesA = calculateString(A)
+    valuesB = calculateString(B)
+    
+    result = []
+    ht = {}
+    for b in valuesB:
+        if b not in ht:
+            num = 0
+            for a in valuesA:
+                if a < b:
+                    num += 1
+            ht[b] = num
+        else:
+            num = ht[b]
+        result.append(num)
+    
+    return result
+
+def generate_random_sentence(num_words=10000, max_len_word=10, min_len_word=1):
+    sentence = ""
+    for i in xrange(num_words):
+        len_word = random.randint(min_len_word, max_len_word)
+        sentence += ''.join(random.choice(string.ascii_lowercase) for _ in range(len_word))
+        if i < num_words - 1:
+            sentence += " "
+    return sentence
+
+    
+if __name__ == "__main__":
+    A = generate_random_sentence(num_words=10000)
+    B = generate_random_sentence(num_words=10000)
+    
+    t1, sol1 = timeFunction(solution, A, B)
+    t2, sol2 = timeFunction(solution_faster, A, B)
+    
+    print "Time taken with slow algorithm: {} seconds".format(t1)
+    print "Time taken with fast algorithm: {} seconds".format(t2)
+    
+    print "Both solution give the same result? {}".format("YES" if sol1 == sol2 else "NO")
---
+++ findLargestSubarray_Google.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python2
+
+"""
+Easy challenge for an internship at Google. Took me 10 min to write the first part.
+
+Array X is greater than array Y if the first non-matching element in both arrays has a
+grater value in X than in Y. For example, [1,2,4,3,5] > [1,2,3,4,5] (because the first
+element that does not match, 4 vs 3, is larger for the first list)
+
+A contiguous subarray is defined by an interval of the indices, or in other words, a
+subarray that has consecutive indexes.
+
+Write a function Solution(A, K) that given a list A consisting of N integers, and an
+integer K, returns the largest contiguous subarray of length K from all the contiguous
+subarrays of length K.
+
+Example:
+    A = [1, 4, 3, 2, 5], K = 3
+    Returns [4, 3, 2] because there are 3 subarrays: [1, 4, 3], [4, 3, 2] and [3, 2, 5]
+                              and [4, 3, 2] is the largest.
+
+Constraints: 1<=K<=N<=100, 1<=A[J]<=1000, A contains N distinct numbers
+
+This last constraint is weird, because it means that we only care about the first number
+of the subarray, not about any other. Therefore, I solved the algorithm in 2 ways, in one
+ignoring this constraint (solution) and in the other following it (solution2).
+"""
+
+def solution2(A, K):
+    # If A has only distinct elements, the problem is trivial,
+    # we just have to find the largest number that appears in
+    # position <= len(A) - K, and return the array with such
+    # element and the next K - 1 numbers in the list
+    
+    # Find and return subarray with largest first number
+    maxNum = 0
+    maxIdx = -1
+    for i in range(len(A) - K + 1):
+        if maxNum < A[i]:
+            maxNum = A[i]
+            maxIdx = i
+    
+    return A[maxIdx:maxIdx + K]
+
+def compare_As(A1, A2):
+    # Return True if A >= B and False if B < A
+    for i in range(len(A1)):
+        if A1[i] > A2[i]:
+            return False
+        elif A1[i] < A2[i]:
+            return True
+    return False
+
+def solution(A, K):
+    # In this solution, I AM ASSUMING THERE MAY BE NOT DISTINCT ELEMENTS IN A
+    # unlike the problem suggests. To see the solution assuming that ther may
+    # not be distinct elements in A, see solution2.
+    
+    # Make list of potential largest contiguous subarrays
+    maxNum = 0
+    maxIdx = -1
+    listA = []
+    for i in range(len(A) - K + 1):
+        if maxNum <= A[i]:
+            maxNum = A[i]
+            listA.append(A[i:i + K])
+    
+    # Iterate through all arrays to find largest contiguous subarray
+    maxA = None
+    for a in listA:
+        if a[0] < maxNum:
+            continue
+        if maxA is None or compare_As(maxA, a):
+            maxA = a
+    
+    return maxA
+
+def test(case, num):
+    print ">>", case
+    print "  ", solution(case, num)
+    print
+
+
+if __name__ == "__main__":
+    test1 = [0, 1, 2, 3, 4, 5, 6, 7]
+    test2 = [7, 6, 5, 4, 3, 2, 1, 0]
+    test3 = [0, 1, 2, 3, 2, 1, 0, 8]
+    test4 = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 0, 1, 2, 3, 4, 5]
+
+    test(test1, 4)
+    test(test2, 4)
+    test(test3, 4)
+    test(test4, 4)
